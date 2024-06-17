b5b42aa7c72e7e330a023afe68bc3f70f7f8dd03
KoolMoh <justmohammad66@gmail.com>
2024-06-17 04:45:30 +0300

    Add .gitignore

--- src/constants_/custom_client.ts
+++ src/constants_/custom_client.ts
@@ -1,6 +1,6 @@
 import { Client, Collection } from "discord.js";
 import { commandHandler } from "./command_handlers";
 
-export default class customClient extends Client {
+export default class CustomClient extends Client {
     commmands = new Collection<string, commandHandler>();
 }
